0acfce94aefc386ae844c4aa3a20a63fee96886c
KemoyQW <kdoyley@qualityworkscg.com>
2020-12-23 16:22:12 -0500

    Added ability to switch environments

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,12 @@
+const url = require('./urls')
+
+const ENV = process.env.ENV
+
+if(!ENV || !['PRD', 'DEV'].includes(ENV)){
+    console.log('Please use following format for running test: ENV=QA')
+    process.exit()
+}
+
 exports.config = {
     //
     // ====================
@@ -90,7 +99,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://qw-test-store-prod.netlify.app/', //'https://the-internet.herokuapp.com/',
+    baseUrl: url[process.env.ENV],
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
